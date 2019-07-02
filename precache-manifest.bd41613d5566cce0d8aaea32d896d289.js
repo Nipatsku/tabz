b5e1cfa3082a6443597b8a6bee7c5fb47d4e0e9e
EAE Hr Keinänen <n.keinaenen@reinhausen.com>
2019-07-02 13:55:39 +0200

    Updates

---
+++ precache-manifest.bd41613d5566cce0d8aaea32d896d289.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72ca2b9098ebd74b05fc",
+    "url": "/tabz/static/js/runtime~main.72ca2b90.js"
+  },
+  {
+    "revision": "2b6f2ac91f707ac7e3aa",
+    "url": "/tabz/static/js/main.2b6f2ac9.chunk.js"
+  },
+  {
+    "revision": "b655df69876aa1165f60",
+    "url": "/tabz/static/js/1.b655df69.chunk.js"
+  },
+  {
+    "revision": "2b6f2ac91f707ac7e3aa",
+    "url": "/tabz/static/css/main.1cdfe21d.chunk.css"
+  },
+  {
+    "revision": "81c8204d0f6afbe7917251962871959d",
+    "url": "/tabz/index.html"
+  }
+];
