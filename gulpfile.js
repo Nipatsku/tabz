7774a7c32794de5eb02a3b8f69ad05d688185c46
Nipatsku <niilo.keinanen@hotmail.com>
2019-07-08 10:29:28 +0200

    Improve gulp syntax

--- gulpfile.js
+++ gulpfile.js
@@ -5,63 +5,59 @@ var fs = require('fs')
 
 var content = require('./content/content.js')
 const buildContentPath = './public/content/'
-function cleanContent() {
+
+
+
+const buildClean = () => new Promise(function(resolve, reject) {
     fs.readdir(buildContentPath, (err, files) => {
         if (err) throw err
         for (const file of files) {
             fs.unlinkSync(`${buildContentPath}${file}`)
         }
     })
-}
-function buildContent() {
-    return new Promise(function(resolve, reject) {
-        cleanContent()
-        setTimeout(() => {
-            var songs = content.songs
-            // Write out song contents to individual files.
-            var list = []
-            for (var song of songs) {
-                const { name, artist } = song
-                if (name.length == 0)
-                    continue
-                
-                const fileName = `${artist}_${name}`
-                    .replace(/\s/g, '_')
-                    .replace(/,|\'|\.|\(|\)|\:|\&/g, '')
-                    .toLowerCase()
-                    + '.json'
-                fs.writeFileSync(
-                    `${buildContentPath}${fileName}`,
-                    JSON.stringify(song)
-                )
-                list.push({
-                    name,
-                    artist,
-                    url: `content/${fileName}`
-                })
-            }
-            // Write song list.
-            fs.writeFileSync(
-                `${buildContentPath}list.json`,
-                JSON.stringify(list)
-            )
-
-            resolve()
-        }, 500)
-    })
-}
-function watchContent() {
-    buildContent()
-        .then(() => {const watcher = gulp.watch(['content/content.js'])
-            console.log('Watching content.')
-            watcher.on('change', function(path, stats) {
-                console.log('Rebuilding content...')
-                buildContent()
-                    .then(() => console.log('Content ready.'))
-            })
+    resolve()
+})
+gulp.task('build-clean', buildClean)
+const buildContent = () => new Promise(function(resolve, reject) {
+    var songs = content.songs
+    // Write out song contents to individual files.
+    var list = []
+    for (var song of songs) {
+        const { name, artist } = song
+        if (name.length == 0)
+            continue
+        
+        const fileName = `${artist}_${name}`
+            .replace(/\s/g, '_')
+            .replace(/,|\'|\.|\(|\)|\:|\&/g, '')
+            .toLowerCase()
+            + '.json'
+        fs.writeFileSync(
+            `${buildContentPath}${fileName}`,
+            JSON.stringify(song)
+        )
+        list.push({
+            name,
+            artist,
+            url: `content/${fileName}`
         })
-}
-
-exports.build = buildContent
-exports.watch = watchContent
-exports.default = buildContent
+    }
+    // Write song list.
+    fs.writeFileSync(
+        `${buildContentPath}list.json`,
+        JSON.stringify(list)
+    )
+    resolve()
+})
+gulp.task('build-content', gulp.series('build-clean', buildContent))
+const buildWatch = () => new Promise(function(resolve, reject) {
+    const watcher = gulp.watch(['content/content.js'])
+    console.log('Watching content.')
+    watcher.on('change', function(path, stats) {
+        console.log('Rebuilding content...')
+        buildContent()
+            .then(() => console.log('Content ready.'))
+    })
+})
+gulp.task('build-watch', gulp.series('build-content', buildWatch))
+gulp.task('default', buildContent)
