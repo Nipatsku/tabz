b9f42498084920b9fdf5171be1fd296b94a4e508
Nipatsku <niilo.keinanen@hotmail.com>
2019-07-08 13:48:50 +0200

    Write test for list.json

--- test/unit-tests/build.test.ts
+++ test/unit-tests/build.test.ts
@@ -10,7 +10,21 @@ describe('public/content', function() {
         // Read 'public/content/list.json'.
         const listRaw = fs.readFileSync('public/content/list.json')
         const list = JSON.parse(listRaw)
-        // Verify correct structure of JSON object.
+        // 'list' should be an Array of 'SongInfo' objects.
+        expect(list).to.have.property('length')
+        const len = list.length
+        for (let i = 0; i < len; i ++) {
+            const item = list[i]
+
+            expect(item).to.have.property('name')
+            expect(item.name).to.be.a('string')
+            
+            expect(item).to.have.property('artist')
+            expect(item.artist).to.be.a('string')
+
+            expect(item).to.have.property('url')
+            expect(item.url).to.be.a('string')
+        }
     })
 })
 
