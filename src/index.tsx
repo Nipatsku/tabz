484aa38cca64222da2a56991ed3b96837f3a5f1c
Nipatsku <niilo.keinanen@hotmail.com>
2019-07-12 14:41:41 +0200

    Add bg

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,36 @@ import { render } from "react-dom";
 import { StartMenu } from "./components/startMenu/StartMenu"
 
 import "./styles.css";
+import { url } from "inspector";
 
-class App extends React.Component {
-  render() {
-    return (
-      <StartMenu />
-    )
-  }
+interface Props {}
+interface State {
+    bgUrl?: string
+}
+class App extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props)
+        fetch("bg.png")
+            .then((data) => {
+                this.setState({
+                    // bgUrl: data.url
+                })
+            })
+        this.state = {}
+    }
+    render() {
+        const { bgUrl } = this.state
+        return <div
+            style={{
+                backgroundImage: bgUrl ? `url(${bgUrl})` : "",
+                backgroundSize: "auto 100%",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat"
+            }}
+        >
+            <StartMenu />
+        </div>
+    }
 }
 
 const rootElement = document.getElementById("root");
